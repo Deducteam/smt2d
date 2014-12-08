649b426060c498c2b16d5f436ab71f4fd6696fdc
Frédéric Gilbert <frederic.a.gilbert@inria.fr>
2014-12-08 14:43:03 +0100

    fix check.ml

--- check.ml
+++ check.ml
@@ -1,16 +1,21 @@
 open Printf
  
-let umsg = "Usage: check <file>"
+let umsg = "Usage: check <files>"
+
+let files = ref []
 
 let argspec = []
+
 let check_file file = 
+  fprintf stdout "checking file %s: " file;
   Context.check_parse file;
-  Context.check_context file;  
-  fprintf stdout "OK"
+  Context.check_context file;
+  fprintf stdout "OK\n"
   
 let () =
   try
-    Arg.parse argspec check_file umsg;
+    Arg.parse argspec (fun f -> files := f :: !files) umsg;
+    List.iter check_file !files;
   with
   | Error.Lexer_error (s, l, c) -> 
      Error.print_location_error l c (sprintf "Unexpected character '%s'"s)
