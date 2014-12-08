dc01589d75bb15751ecb53648c4b8c227451fb42
Frédéric Gilbert <frederic.a.gilbert@inria.fr>
2014-12-08 20:01:50 +0100

    concrete to abstract finished

--- abstract.ml
+++ abstract.ml
@@ -77,6 +77,20 @@ module FunsMap =
       let compare = Pervasives.compare
     end)
 
+module VarsMap =
+  Map.Make
+    (struct
+      type t = variable
+      let compare = Pervasives.compare
+    end)
+
+module VarsSet =
+  Set.Make
+    (struct
+      type t = variable
+      let compare = Pervasives.compare
+    end)
+
 (* one can infer a signature from a signature by removing all definitions *)
 type signature = {
   sorts: sort_data SortsMap.t;
@@ -120,10 +134,13 @@ let number num =
 let sort_symbol id =
   id
 
-let fun_symbol sym =
-  Identifier_fun (sym, [])
+let fun_symbol id =
+  Identifier_fun id
 
 let sort_parameter sym =
+  sym
+
+let variable sym =
   sym
 
 let logic_name sym =
@@ -145,6 +162,68 @@ let rec parametric_sort pars csort =
      else Par_sort (sort_symbol (sym, []), [])
   | Concrete.Sort (id, csorts) ->
      Par_sort (sort_symbol id, List.map (parametric_sort pars) csorts)
+
+let attribute (key, opt) =
+  key, opt
+
+let sorted_var (sym, csort) =
+  variable sym, sort csort
+
+let rec var_binding vars_env (sym, cterm) =
+  variable sym, term vars_env cterm
+
+and term vars_env cterm =
+  let map f opt = match opt with None -> None | Some a -> Some (f a) in
+  match cterm with
+  | Concrete.Spec_constant_term const ->
+     App (Spec_constant_fun const, None, [])
+  | Concrete.Qual_identifier_term ((sym, []), opt) ->
+     let var = variable sym in
+     begin 
+       match opt with
+       | None ->
+	  if VarsSet.mem var vars_env
+	  then Var var
+	  else App (fun_symbol (sym, []), None, []) 
+       | Some csort ->
+	  App (fun_symbol (sym, []), Some (sort csort), []) end
+  | Concrete.Qual_identifier_term (id, opt) ->
+     App (fun_symbol id, map sort opt, [])
+  | Concrete.App_term (((sym, []), opt), cterms) ->
+     let var = variable sym in
+     begin 
+       match opt, cterms with
+       | None, [] ->
+	  if VarsSet.mem var vars_env
+	  then Var var
+	  else App (fun_symbol (sym, []), None, List.map (term vars_env) cterms)
+       | _, _ ->
+	  App (fun_symbol (sym, []), map sort opt, List.map (term vars_env) cterms) end
+  | Concrete.App_term ((id, opt), cterms) ->
+     App (fun_symbol id, map sort opt, List.map (term vars_env) cterms)
+  | Concrete.Let_term (var_bindings, cterm) ->
+     let bindings = List.map (var_binding vars_env) var_bindings in
+     Let 
+       (bindings, 
+	term 
+	  (List.fold_left (fun vars_env (var, _) -> VarsSet.add var vars_env) vars_env bindings) 
+	  cterm)
+  | Concrete.Forall_term (csorted_vars, cterm) ->
+     let sorted_vars = List.map sorted_var csorted_vars in
+     Forall 
+       (sorted_vars, 
+	term 
+	  (List.fold_left (fun vars_env (var, _) -> VarsSet.add var vars_env) vars_env sorted_vars)
+	  cterm)
+  | Concrete.Exists_term (csorted_vars, cterm) ->
+     let sorted_vars = List.map sorted_var csorted_vars in
+     Exists
+       (sorted_vars, 
+	term 
+	  (List.fold_left (fun vars_env (var, _) -> VarsSet.add var vars_env) vars_env sorted_vars)
+	  cterm)
+  | Concrete.Attributed_term (cterm, cattributes) ->
+     Attributed (term vars_env cterm, List.map attribute cattributes)
 
 (* *** CONSTANTS *** *)
 
@@ -256,6 +335,33 @@ let rec pop n stack =
   | _, current :: other ->
      pop (n-1) other
 
+let rec run_in_line_definitions signature term =
+  match term with
+  | Var _ -> signature, term
+  | App (fun_sym, opt, terms) ->
+     let newsignature, newterms_rev = 
+       List.fold_left 
+	 (fun (signature, newterms_rev) term ->
+	  let newsignature, newterm = run_in_line_definitions signature term in
+	  newsignature, newterm :: newterms_rev) (signature, []) terms in
+     newsignature, App (fun_sym, opt, List.rev newterms_rev)
+  | Let (bindings, term) -> 
+     let newsignature, newterm = run_in_line_definitions signature term in
+     newsignature, Let (bindings, newterm)
+  | Forall (sorted_vars, term) ->
+     let newsignature, newterm = run_in_line_definitions signature term in
+     newsignature, Forall (sorted_vars, newterm)
+  | Exists (sorted_vars, term) ->
+     let newsignature, newterm = run_in_line_definitions signature term in
+     newsignature, Exists (sorted_vars, newterm)
+  | Attributed (term, attributes) ->
+     let attribute_names, _ = List.split attributes in
+     if List.mem ":named" attribute_names
+     then raise Error.Not_implemented
+     else 
+       let newsignature, newterm = run_in_line_definitions signature term in
+       newsignature, Attributed (newterm, attributes)       
+
 let run_command command stack =
   (* pop function alows us to assert assert that the stack has a head *)
   let signature, assertions, sets = 
@@ -277,16 +383,32 @@ let run_command command stack =
      let par_sort = parametric_sort pars tau in
      (add_sort sort_sym (Sort_definition (pars, par_sort)) signature, assertions) :: sets
   | Concrete.Declare_fun (sym, csorts, csort) ->
-     let fun_sym = fun_symbol sym in
+     let fun_sym = fun_symbol (sym, []) in
      let par_sorts = List.map (parametric_sort []) csorts in
      let par_sort = parametric_sort [] csort in
      (add_fun fun_sym (Fun_declaration [[], par_sorts, par_sort]) signature, assertions) :: sets
-  | Concrete.Define_fun (sym, sorted_vars, s, t) ->
-     raise Error.Not_implemented
-  | Concrete.Assert t ->
-     raise Error.Not_implemented
-  | Concrete.Get_value ts ->
-     raise Error.Not_implemented
+  | Concrete.Define_fun (sym, csorted_vars, csort, cterm) ->
+     let fun_sym = fun_symbol (sym, []) in
+     let sorted_vars = List.map sorted_var csorted_vars in
+     let sort = sort csort in
+     let vars_env = 
+       List.fold_left 
+	 (fun vars_env (var, sort) -> VarsSet.add var vars_env) VarsSet.empty sorted_vars in
+     let term = term vars_env cterm in
+     let newsignature, newterm = run_in_line_definitions signature term in
+     (add_fun fun_sym (Fun_definition (sorted_vars, sort, newterm)) newsignature, assertions) :: sets
+  | Concrete.Assert cterm ->
+     let term = term VarsSet.empty cterm in
+     let newsignature, newterm = run_in_line_definitions signature term in
+     (newsignature, newterm :: assertions) :: sets
+  | Concrete.Get_value cterms ->
+     let terms = List.map (term VarsSet.empty) cterms in
+     let newsignature =
+       List.fold_left 
+	 (fun signature term -> 
+	  let newsignature, _ = run_in_line_definitions signature term in
+	  newsignature) signature terms in
+     (newsignature, assertions) :: sets
   | Concrete.Set_logic _ ->
      raise Logic_error
   | _ -> stack
