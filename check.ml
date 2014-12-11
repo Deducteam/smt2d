75d4f0cf880717f11c770c379d85c967397c17b2
Frederic Gilbert <frederic.a.gilbert@inria.fr>
2014-12-11 09:25:36 +0100

    move run to run_script

--- check.ml
+++ check.ml
@@ -9,12 +9,12 @@ let argspec = []
 let check_get_script file = 
   let chan = open_in file in
   let lexbuf = Lexing.from_channel chan in
-  let _ = Run.get_script lexbuf in ()
+  let _ = Run_script.get_script lexbuf in ()
 	  
 let check_get_contexts file =
   let chan = open_in file in
   let lexbuf = Lexing.from_channel chan in
-  let _ = Run.get_contexts lexbuf in ()
+  let _ = Run_script.get_contexts lexbuf in ()
 
 let check_file file = 
   fprintf stdout "checking file %s: " file;
